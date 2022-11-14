a65bb92e70aadfa0ed12229d85a993951dc1bfea
jeffr <jeffr@10.100.209.16>
2022-11-14 09:32:12 -0600

    PE 6.37

---
+++ JeffreyHoyt_2200_PE6.37/src/Exercise06_37.java
@@ -0,0 +1,22 @@
+/* Jeffrey Hoyt
+ * [F]11/11/2022
+ */
+import java.util.Scanner;
+public class Exercise06_37 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter an integer: ");
+		int number = input.nextInt();
+		System.out.print("Enter the number width: ");
+		int width = input.nextInt();
+		System.out.println("The formatted number is " + format(number,width));
+	}
+	public static String format(int n, int w) {
+		String result = n + "";
+		int len = w - result.length();
+		for(int i = 0;i <= len;i++) {
+			result = "0" + result;
+		}
+		return result;
+	}
+}
